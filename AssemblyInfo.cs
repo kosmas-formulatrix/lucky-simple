b096927cf675deba8b73a14c3d6a3d4b0aac6fa2
Tyler <tyler@formulatrix.com>
2013-08-27 08:20:45 +0700

    first commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle( "Test " )]
+[assembly: AssemblyDescription( "Test  Control Software" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Formulatrix, Inc." )]
+[assembly: AssemblyProduct( "Test  Control Software" )]
+[assembly: AssemblyTrademark( "January, 2012" )]
+[assembly: AssemblyCopyright( "Copyright 2008, 2009, 2010, 2011, 2012 Formulatrix, Inc." )]
+[assembly: AssemblyCulture( "" )]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
